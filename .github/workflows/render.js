61849d12296cc62112d6456a88fb87fd39febb69
ashelar-spec <ashelar@webuy.com>
2026-07-30 15:24:46 +0530

    Create render.js

---
+++ .github/workflows/render.js
@@ -0,0 +1,20 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+(async () => {
+  const htmlContent = fs.readFileSync('table.html', 'utf8');
+
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
+  const page = await browser.newPage();
+
+  await page.setViewport({ width: 1400, height: 900, deviceScaleFactor: 2 });
+  await page.setContent(htmlContent, { waitUntil: 'networkidle0' });
+
+  // Crop directly to the <table> element for zero extra whitespace
+  const element = await page.$('table');
+  await element.screenshot({ path: 'output.png' });
+
+  await browser.close();
+})();
